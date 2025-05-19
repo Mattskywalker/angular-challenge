0ca5c04ef20f67e38aa15eec096b11d0fff7db9f
Mattskywalker <mateusejesus@hotmail.com.br>
2025-05-19 10:55:17 -0300

    home Page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { CostumerService, Customer } from 'src/app/core/services/costumer.service';
+import { CustomerService, Customer } from 'src/app/core/services/customer.service';
+
+const cpf = '881.628.750-00';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  customer: Customer;
+  inputCpf: string = '';
 
-  costumer: Customer
+  showError: boolean = false;
 
-  constructor(private costumerService: CostumerService) { }
+  constructor(private customerService: CustomerService) {}
 
   ngOnInit() {
-
-    this.costumer = this.costumerService.getCostumer('')
+    this.searchCustomer();
+    console.log(this.customer);
   }
 
+  clearInput() {
+    this.inputCpf = '';
+  }
+
+  searchCustomer() {
+    this.customerService.getCostumer(cpf).subscribe({
+      next: (customer) => {
+        this.customer = customer;
+        this.showError = false;
+      },
+      error: (e) => {
+        console.error('Erro ', e);
+        this.showError = true;
+        this.clearInput();
+      },
+    });
+  }
+
+  getAccountLabel(label: string) {
+    return `Conta ${label.toLowerCase()}`;
+  }
+
+  get registrationStatus() {
+    const word = this.customer.registrationStatus;
+    return word ? word[0].toUpperCase() + word.slice(1).toLowerCase() : '';
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material';
+import { LayoutModule } from '../../layout/layout.module';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    SharedModule,
+    FormsModule,
+    MatIconModule,
+    LayoutModule,
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
